25b95baece6140a894a71f80c478de89d7eb2850
reinaldomaslim <reinaldomaslim@gmail.com>
2020-07-19 16:23:06 +0800

    update db, added email sub

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from flask import Flask, jsonify, render_template, request
+import sqlite3
+from sqlite3 import Error
+from datetime import datetime
+from db_handler import *
 
-# TODO: Add static file paths properly
+DATABASE_NAME = r"./sqlite.db"
+
 app = Flask(__name__,
             template_folder="html/index",
             static_url_path="",
             static_folder="html/index")
 
-
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-
 @app.route("/subscribe", methods=["POST"])
 def subscribe():
     # TODO: Add email subscription
-    print(f"Email subscribed: {request.form['email']}")
-    return jsonify(success=True)
 
+    print(f"Email subscribed: {request.form['email']}")    
+    conn = create_connection(DATABASE_NAME)
+    user = [request.form['email'], 0, datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d"), 0, 0]        
+    insert_user(conn, user)
+    
+    return jsonify(success=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- db_handler.py
+++ db_handler.py
@@ -2,11 +2,10 @@ import sqlite3
 from sqlite3 import Error
 from datetime import datetime
 
-
 def create_connection(db_file):
     conn = None
     try:
-        conn = sqlite3.connect(db_file)
+        conn = sqlite3.connect(db_file, timeout=10)
         return conn
     except Error as e:
         print(e)
@@ -19,47 +18,29 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
-def insert_mail(conn, msg):
-    sql = ''' INSERT INTO mail(email,pro)
-              VALUES(?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, msg)
-    conn.commit()
-    return cur.lastrowid
-
 def insert_user(conn, msg):
-    sql = ''' INSERT INTO users(email,purchased,created,pid,lastSent,unsubscribe,deactivated)
-              VALUES(?,?,?,?,?,?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, msg)
-    conn.commit()
-    return cur.lastrowid
-
-def insert_problem(conn, msg):
-    sql = ''' INSERT INTO problems(description, solution)
-              VALUES(?, ?)
+    sql = ''' INSERT OR IGNORE INTO users(email,purchased,created,lastSent,unsubscribe,deactivated)
+              VALUES(?,?,?,?,?,?) 
               '''
     cur = conn.cursor()
     cur.execute(sql, msg)
     conn.commit()
     return cur.lastrowid
 
-def update_mail(conn, msg):
-    sql = ''' UPDATE mail
-              SET email = ? ,
-                  pro = ? 
-              WHERE id = ?'''
-
+def insert_problem(conn, msg):
+    sql = ''' INSERT OR IGNORE INTO problems(filename)
+              VALUES(?)
+              '''
     cur = conn.cursor()
     cur.execute(sql, msg)
     conn.commit()
+    return cur.lastrowid
 
 def update_user(conn, msg):
     sql = ''' UPDATE users
               SET email = ? ,
                   purchased = ?,
                   created = ?,
-                  pid = ?,
                   lastSent = ?,
                   unsubscribe = ?,
                   deactivated =? 
@@ -71,8 +52,7 @@ def update_user(conn, msg):
 
 def update_problem(conn, msg):
     sql = ''' UPDATE problems
-              SET description = ?,
-                  solution = ?
+              SET filename = ?
               WHERE id = ?'''
 
     cur = conn.cursor()
@@ -106,52 +86,46 @@ def select_all(conn, table):
 if __name__ == '__main__':
     DATABASE_NAME = r"./sqlite.db"
 
-    sql_create_mails_table = """CREATE TABLE IF NOT EXISTS mails (
-                                        id integer PRIMARY KEY,
-                                        email text NOT NULL,
-                                        pro boolean
-                                    ); """
-
     sql_create_users_table = """CREATE TABLE IF NOT EXISTS users (
                                     id integer PRIMARY KEY,
                                     email text,
                                     purchased boolean NOT NULL,
                                     created datetime,
-                                    pid integer NOT NULL,
                                     lastSent datetime,
                                     unsubscribe boolean NOT NULL,
                                     deactivated boolean NOT NULL,
-                                    FOREIGN KEY (pid) REFERENCES problems (pid)
-                                );"""
+                                    UNIQUE(email)
+                                    )
+                                    ;"""
 
     sql_create_problems_table = """CREATE TABLE IF NOT EXISTS problems (
                                     pid integer PRIMARY KEY,
-                                    description text,
-                                    solution text
+                                    filename text,
+                                    UNIQUE(filename)
                                 );"""
 
     conn = create_connection(DATABASE_NAME)
-
-    delete_table(conn, 'mails')
-    delete_table(conn, 'users')
-    delete_table(conn, 'problems')
+    try:
+        delete_table(conn, 'users')
+        delete_table(conn, 'problems')
+    except:
+        pass
 
     if conn is not None:
-        create_table(conn, sql_create_mails_table)
         create_table(conn, sql_create_users_table)
         create_table(conn, sql_create_problems_table)
     else:
         print("Error! cannot create the database connection.")
 
     with conn:
-        prob1 = ['find x', 'there']
-        prob2 = ['find y', 'here']
+        prob1 = ['./compendium/findx.pdf']
+        prob2 = ['./compendium/findy.pdf']
 
         insert_problem(conn, prob1)
         insert_problem(conn, prob2)
 
-        user1 = ['reinaldomaslim@gmail.com', 0, datetime.now().strftime("%Y-%m-%d"), 0, datetime.now().strftime("%Y-%m-%d"), 0, 0]
-        user2 = ['e0403962@u.nus.edu', 0, datetime.now().strftime("%Y-%m-%d"), 0, datetime.now().strftime("%Y-%m-%d"), 0, 0]
+        user1 = ['reinaldomaslim@gmail.com', 0, datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d"), 0, 0]
+        user2 = ['e0403962@u.nus.edu', 0, datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d"), 0, 0]
         
         insert_user(conn, user1)
         insert_user(conn, user2)
