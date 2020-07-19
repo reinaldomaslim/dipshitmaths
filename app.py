c637b1c1149aec5f309b59e12ecd5073bea265ac
Yan Paing Oo <yanpaioo94@gmail.com>
2020-07-19 18:12:50 +0800

    UPD: Update db_handler

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
-from flask import Flask, jsonify, render_template, request
-import sqlite3
-from sqlite3 import Error
+from flask import Flask, jsonify, render_template, request, g
+from db_handler import DBHandler
 from datetime import datetime
-from db_handler import *
 
 DATABASE_NAME = r"./sqlite.db"
+
+
+def get_db_handler():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = DBHandler(DATABASE_NAME)
+    return db
+
 
 app = Flask(__name__,
             template_folder="html/index",
             static_url_path="",
             static_folder="html/index")
 
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
+
 @app.route("/subscribe", methods=["POST"])
 def subscribe():
-    # TODO: Add email subscription
-
-    print(f"Email subscribed: {request.form['email']}")    
-    conn = create_connection(DATABASE_NAME)
     user = [request.form['email'], 0, datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d"), 0, 0]        
-    insert_user(conn, user)
-    
-    return jsonify(success=True)
+
+    try:
+        get_db_handler().insert_user(user)
+        print(f"Email subscribed: {request.form['email']}")
+        return jsonify(success=True)
+    except Exception as e:
+        print(e)
+        return jsonify(success=False)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- db_handler.py
+++ db_handler.py
@@ -2,91 +2,125 @@ import sqlite3
 from sqlite3 import Error
 from datetime import datetime
 
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file, timeout=10)
-        return conn
-    except Error as e:
-        print(e)
-    return conn
 
-def create_table(conn, create_table_sql):
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
+class DBHandler:
 
-def insert_user(conn, msg):
-    sql = ''' INSERT OR IGNORE INTO users(email,purchased,created,lastSent,unsubscribe,deactivated)
-              VALUES(?,?,?,?,?,?) 
+    def __init__(self, db_file):
+        # Create DB
+        try:
+            self.conn = sqlite3.connect(db_file, timeout=10, check_same_thread=False)
+        except Error as e:
+            print(e)
+            exit(0)
+        except Exception as e:
+            print(e)
+            exit(0)
+
+        # Flush tables
+        self.delete_table("users")
+        self.delete_table("problems")
+
+        # Initialize tables and problems
+        self._initialize_tables()
+        self._add_dummy_problems()
+
+    def create_table(self, sql):
+        try:
+            c = self.conn.cursor()
+            c.execute(sql)
+        except Error as e:
+            print(e)
+        except Error as e:
+            print(e)
+
+    def insert_problem(self, msg):
+        sql = ''' INSERT OR IGNORE INTO problems(filename)
+                  VALUES(?)
               '''
-    cur = conn.cursor()
-    cur.execute(sql, msg)
-    conn.commit()
-    return cur.lastrowid
+        cur = self.conn.cursor()
+        cur.execute(sql, msg)
+        self.conn.commit()
+        return cur.lastrowid
 
-def insert_problem(conn, msg):
-    sql = ''' INSERT OR IGNORE INTO problems(filename)
-              VALUES(?)
+    def insert_user(self, msg):
+        sql = ''' INSERT OR IGNORE INTO users(email,purchased,created,lastSent,unsubscribe,deactivated)
+                  VALUES(?,?,?,?,?,?) 
               '''
-    cur = conn.cursor()
-    cur.execute(sql, msg)
-    conn.commit()
-    return cur.lastrowid
+        cur = self.conn.cursor()
+        cur.execute(sql, msg)
+        self.conn.commit()
+        return cur.lastrowid
 
-def update_user(conn, msg):
-    sql = ''' UPDATE users
-              SET email = ? ,
-                  purchased = ?,
-                  created = ?,
-                  lastSent = ?,
-                  unsubscribe = ?,
-                  deactivated =? 
-              WHERE id = ?'''
-              
-    cur = conn.cursor()
-    cur.execute(sql, msg)
-    conn.commit()
+    def select_all(self, table):
+        cur = self.conn.cursor()
+        cur.execute("SELECT * FROM " + table)
+        rows = cur.fetchall()
+        return rows
 
-def update_problem(conn, msg):
-    sql = ''' UPDATE problems
-              SET filename = ?
-              WHERE id = ?'''
+    def update_problem(self, msg):
+        sql = ''' UPDATE problems
+                SET filename = ?
+                WHERE id = ?'''
 
-    cur = conn.cursor()
-    cur.execute(sql, msg)
-    conn.commit()
+        cur = self.conn.cursor()
+        cur.execute(sql, msg)
+        self.conn.commit()
 
-def delete_table(conn, table):
-    sql = 'DROP TABLE '+table
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
+    def update_user(self, msg):
+        sql = ''' UPDATE users
+                SET email = ? ,
+                    purchased = ?,
+                    created = ?,
+                    lastSent = ?,
+                    unsubscribe = ?,
+                    deactivated =? 
+                WHERE id = ?'''
 
-def delete_all_rows(conn, table):
-    sql = 'DELETE FROM '+table
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
+        cur = self.conn.cursor()
+        cur.execute(sql, msg)
+        self.conn.commit()
 
-def delete(conn, table, id):
-    sql = 'DELETE FROM '+table+' WHERE id=?'
-    cur = conn.cursor()
-    cur.execute(sql, (id,))
-    conn.commit()
+    def delete_table(self, table):
+        try:
+            sql = 'DROP TABLE '+table
+            cur = self.conn.cursor()
+            cur.execute(sql)
+            self.conn.commit()
+        except Error as e:
+            print(e)
+        except Exception as e:
+            print(e)
 
-def select_all(conn, table):
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM "+table)
-    rows = cur.fetchall()
-    return rows
+    def delete_all_rows(self, table):
+        sql = 'DELETE FROM ' + table
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        self.conn.commit()
 
-if __name__ == '__main__':
-    DATABASE_NAME = r"./sqlite.db"
+    def delete(self, table, id):
+        sql = 'DELETE FROM ' + table + ' WHERE id=?'
+        cur = self.conn.cursor()
+        cur.execute(sql, (id,))
+        self.conn.commit()
 
-    sql_create_users_table = """CREATE TABLE IF NOT EXISTS users (
+    def _add_dummy_problems(self):
+        with self.conn:
+            # Add dummy problems
+            prob1 = ['./compendium/findx.pdf']
+            prob2 = ['./compendium/findy.pdf']
+            self.insert_problem(prob1)
+            self.insert_problem(prob2)
+
+            # Add dummy users
+            user1 = ['reinaldomaslim@gmail.com', 0, datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d"), 0, 0]
+            user2 = ['e0403962@u.nus.edu', 0, datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d"), 0, 0]
+            self.insert_user(user1)
+            self.insert_user(user2)
+
+            print(self.select_all("users"))
+
+    def _initialize_tables(self):
+        sql_create_users_table = """CREATE TABLE IF NOT EXISTS users (
                                     id integer PRIMARY KEY,
                                     email text,
                                     purchased boolean NOT NULL,
@@ -97,40 +131,11 @@ if __name__ == '__main__':
                                     UNIQUE(email)
                                     )
                                     ;"""
+        self.create_table(sql_create_users_table)
 
-    sql_create_problems_table = """CREATE TABLE IF NOT EXISTS problems (
+        sql_create_problems_table = """CREATE TABLE IF NOT EXISTS problems (
                                     pid integer PRIMARY KEY,
                                     filename text,
                                     UNIQUE(filename)
-                                );"""
-
-    conn = create_connection(DATABASE_NAME)
-    try:
-        delete_table(conn, 'users')
-        delete_table(conn, 'problems')
-    except:
-        pass
-
-    if conn is not None:
-        create_table(conn, sql_create_users_table)
-        create_table(conn, sql_create_problems_table)
-    else:
-        print("Error! cannot create the database connection.")
-
-    with conn:
-        prob1 = ['./compendium/findx.pdf']
-        prob2 = ['./compendium/findy.pdf']
-
-        insert_problem(conn, prob1)
-        insert_problem(conn, prob2)
-
-        user1 = ['reinaldomaslim@gmail.com', 0, datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d"), 0, 0]
-        user2 = ['e0403962@u.nus.edu', 0, datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d"), 0, 0]
-        
-        insert_user(conn, user1)
-        insert_user(conn, user2)
-
-        users = select_all(conn, 'users')   
-        print(users)
-
-        # delete(conn, 'users', 1)
+                                    );"""
+        self.create_table(sql_create_problems_table)
