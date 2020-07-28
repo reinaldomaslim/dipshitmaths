5ab3a4c79c29551569aaed5f8b254a067c218d9a
Yan Paing Oo <yanpaioo94@gmail.com>
2020-07-28 22:29:12 +0800

    ADD: Add email confirmation without sending email

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, render_template, request, g
-from db_handler import DBHandler
+from db_handler import DBEngine
 from datetime import datetime
+from uuid import uuid4
 
 DATABASE_NAME = r"./sqlite.db"
+DB_ENGINE = None
 
 
 def get_db_handler():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = DBHandler(DATABASE_NAME)
-    return db
+    global DB_ENGINE
+    if DB_ENGINE is None:
+        DB_ENGINE = DBEngine(DATABASE_NAME)
+    return DB_ENGINE.handler
 
 
 app = Flask(__name__,
@@ -20,22 +22,52 @@ app = Flask(__name__,
 
 @app.route("/", methods=["GET"])
 def index():
+    print("Index request")
     return render_template("index.html")
 
 
 @app.route("/subscribe", methods=["POST"])
 def subscribe():
+    print("Subscribe request")
+    db_handler = get_db_handler()
+    referid = str(uuid4())
     try:
-        engine = get_db_handler()
-        engine.insert_user(
+        db_handler.insert_email_confirmation(
             email=request.form["email"],
-            purchased=False,
-            lastSent=datetime.now(),
-            unsubscribed=False,
-            deactivated=False
-        )
-        print(f"Email subscribed: {request.form['email']}")
+            referid=referid)
+    except Exception as e:
+        print(e)
+        return jsonify(success=False)
+    else:
+        # TODO: Send verification email
+        print(f"Confirmation email added: {request.form['email']}")
         return jsonify(success=True)
+
+
+@app.route("/confirm", methods=["GET"])
+def email_confirmation():
+    print("Email confirmation request")
+    email = request.values.get("email")
+    referid = request.values.get("referid")
+    print(f"Email: {email}")
+    print(f"Referid: {referid}")
+    if email is None or referid is None:
+        return jsonify(success=False)
+
+    db_handler = get_db_handler()
+    try:
+        ret = db_handler.select_email_confirmation(email=email)
+        if ret["referid"] == referid:
+            db_handler.delete_email_confirmation(email=email)
+            db_handler.insert_user(
+                email=email,
+                purchased=False,
+                lastSent=datetime.now(),
+                unsubscribed=False,
+                deactivated=False)
+            return jsonify(success=True)
+        else:
+            return jsonify(success=False)
     except Exception as e:
         print(e)
         return jsonify(success=False)
--- db_handler.py
+++ db_handler.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Table, Column, Integer, String, Boolean, DateTime, MetaData
-from sqlalchemy.sql import select
+from sqlalchemy.sql import select, delete
 from datetime import datetime
 
 metadata = MetaData()
@@ -15,28 +15,37 @@ Users = Table(
     Column("created", DateTime),
     Column("lastSent", DateTime),
     Column("unsubscribed", Boolean, nullable=False),
-    Column("deactivated", Boolean, nullable=False)
-)
+    Column("deactivated", Boolean, nullable=False))
 
 Problems = Table(
     "problems",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("filename", String(length=200), unique=True, nullable=False)
-)
+    Column("filename", String(length=200), unique=True, nullable=False))
+
+EmailConfirmation = Table(
+    "email-confirmation",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String(length=100), nullable=False, unique=True),
+    Column("created", DateTime, nullable=False),
+    Column("referid", String(length=100), nullable=False))
 
 
-class DBHandler:
-
+class DBEngine:
     def __init__(self, db_path):
         self._db_path = db_path
-        self._engine = create_engine(f"sqlite:///{self._db_path}", echo=False)
+        self._engine = create_engine(f"sqlite:///{self._db_path}", echo=True)
         self._conn = self._engine.connect()
         self._users = Users
         self._problems = Problems
+        self._email_confirmation = EmailConfirmation
         metadata.create_all(self._engine)
-
         self._initialize_db()
+
+    @property
+    def handler(self):
+        return self
 
     def _initialize_db(self):
         # Dummy users
@@ -45,20 +54,17 @@ class DBHandler:
             purchased=False,
             lastSent=datetime.now(),
             unsubscribed=False,
-            deactivated=False
-        )
+            deactivated=False)
         self.insert_user(
             email="e0403962@u.nus.edu",
             purchased=False,
             lastSent=datetime.now(),
             unsubscribed=False,
-            deactivated=False
-        )
+            deactivated=False)
 
         # Dummy problems
         self.insert_problem("./compendium/findx.pdf")
         self.insert_problem("./compendium/findy.pdf")
-
         print(self._users_select_all())
 
     def insert_user(self, email, purchased, lastSent, unsubscribed, deactivated):
@@ -68,15 +74,30 @@ class DBHandler:
             created=datetime.now(),
             lastSent=lastSent,
             unsubscribed=unsubscribed,
-            deactivated=deactivated
-        )
+            deactivated=deactivated)
+        self._conn.execute(ins)
+
+    def insert_email_confirmation(self, email, referid):
+        ins = self._email_confirmation.insert().prefix_with("OR IGNORE").values(
+            email=email,
+            created=datetime.now(),
+            referid=referid)
+        self._conn.execute(ins)
+
+    def select_email_confirmation(self, email):
+        ins = select([self._email_confirmation]).where(self._email_confirmation.c.email == email)
+        result = self._conn.execute(ins).fetchone()
+        return dict(result or {})
+
+    def delete_email_confirmation(self, email):
+        ins = self._email_confirmation.delete().where(self._email_confirmation.c.email == email)
         self._conn.execute(ins)
 
     def insert_problem(self, filename):
-        ins = self._problems.insert().prefix_with("OR IGNORE").values(
-            filename=filename
-        )
+        ins = self._problems.insert().prefix_with("OR IGNORE").values(filename=filename)
         self._conn.execute(ins)
 
     def _users_select_all(self):
-        return select([self._users])
+        ins = select([self._users])
+        result = self._conn.execute(ins)
+        return [dict(row) for row in result]
